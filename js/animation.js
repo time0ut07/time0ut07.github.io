d06f70ee730ad4654360b7ad3c1159c106670a31
time0ut07 <aldenchia26@gmail.com>
2025-07-02 22:02:28 +0800

    blog - ctrl_alt_certified v3.02 (disabled new window)

--- js/animation.js
+++ js/animation.js
@@ -202,7 +202,8 @@ document.querySelectorAll('.docsButton').forEach(button => {
       
       const zipUrl = `https://time0ut07.github.io/posts/${docType}/${directory}.html`;
       
-      window.open(zipUrl);
+      // window.open(zipUrl, '_blank');
+      window.location.href = zipUrl;
   });
 });
 
